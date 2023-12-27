027b6638a86e8e1aebd0bb85cbe211f10210fc4f
Oscar Recén Larsson <112632742+oscarrecenlarsson@users.noreply.github.com>
2023-12-27 17:38:30 +0100

    Feat: Pay2Say-17 basic theme

--- frontend/src/components/Disclaimer.tsx
+++ frontend/src/components/Disclaimer.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import * as S from "./Styles";
 
 interface DisclaimerProps {
@@ -5,17 +6,52 @@ interface DisclaimerProps {
 }
 
 export default function Disclaimer(props: DisclaimerProps) {
+  const [isChecked, setIsChecked] = useState(false);
+
   const handleDisclaimerConfirm = () => {
-    props.setShowDisclaimer(false);
+    if (isChecked) {
+      props.setShowDisclaimer(false);
+    } else {
+      alert("Please agree to the terms before proceeding.");
+    }
+  };
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
   };
 
   return (
     <>
       <S.Disclaimer>
-        <S.Text>
-          This is a disclaimer. Please read and confirm.
-          {<button onClick={handleDisclaimerConfirm}>I Agree</button>}
-        </S.Text>
+        <S.DisclaimerText>
+          <h2>Disclaimer</h2>
+          <h3>Viewer discretion is advised</h3>
+          Welcome to Pay2Say, where the classic concept of free speech meets the
+          world of Web3 technology. This websites displays the contents of a
+          smart contract that holds a short text. Given that the text could be
+          anything, and we really mean anything, this website can not take any
+          resposibility for what is displayed. The content displayed on our
+          platform is a reflection of an individual user's views and does not
+          represent the opinions or values of the site itself. Access to this
+          platform is restricted to individuals who are 18 years of age or
+          older. This age requirement is in place to guarantee that users have
+          the maturity and understanding necessary to engage with and contribute
+          to this open and unrestricted smart contract. By using this service,
+          you confirm that you meet this age criterion and are legally capable
+          of agreeing to these terms.
+          <S.CheckboxContainer>
+            <input
+              type="checkbox"
+              id="agreeTermsCheckbox" // Adding an id here
+              checked={isChecked}
+              onChange={handleCheckboxChange}
+            />
+            <S.ClickableLabel htmlFor="agreeTermsCheckbox">
+              I agree to these terms and conditions.
+            </S.ClickableLabel>{" "}
+          </S.CheckboxContainer>
+          {<S.Btn onClick={handleDisclaimerConfirm}>Enter Site</S.Btn>}
+        </S.DisclaimerText>
       </S.Disclaimer>
     </>
   );
--- frontend/src/components/Styles.tsx
+++ frontend/src/components/Styles.tsx
@@ -1,7 +1,19 @@
 import styled from "styled-components";
 
+const primaryDark = "#1C1C1C";
+const primaryLight = "#383838";
+
+const secondaryDark = "#646464";
+const secondaryLight = "#D9D9D9";
+
+const interactiveNormal = "#64FFDA";
+const interactiveHover = "#C0C0C0";
+
+export const Body = styled.body`
+  font-family: Arial, Helvetica, sans-serif;
+`;
+
 export const OutletWrapper = styled.main`
-  // - sum of header and footer height
   min-height: calc(100vh - 128px);
   min-width: 600px;
   height: 100%;
@@ -11,9 +23,15 @@ export const OutletWrapper = styled.main`
   justify-content: center;
   align-items: center;
   gap: 15%;
-  background-color: papayawhip;
   overflow-y: auto;
   margin-top: 64px;
+  background: repeating-linear-gradient(
+    135deg,
+    ${primaryDark},
+    ${primaryDark} 40px,
+    black 40px,
+    black 80px
+  );
 
   @media (max-width: 992px) {
     flex-direction: column;
@@ -31,9 +49,10 @@ export const Header = styled.header`
   flex-direction: row;
   justify-content: space-around;
   align-items: center;
-  background-color: grey;
+  background-color: ${secondaryDark};
   position: fixed;
   top: 0;
+  z-index: 100;
 `;
 
 export const Nav = styled.nav`
@@ -54,11 +73,11 @@ export const Nav = styled.nav`
     li {
       height: 100%;
       width: 64px;
-      background-color: red;
       display: flex;
       justify-content: center;
       align-items: center;
     }
+
     li a {
       height: 100%;
       width: 100%;
@@ -67,29 +86,37 @@ export const Nav = styled.nav`
       align-items: center;
       color: black;
       text-decoration: none;
-      background-color: aqua;
+      transition: background-color 0.3s, color 0.3s;
+
+      &:hover {
+        background-color: ${interactiveHover};
+        color: ${primaryDark};
+      }
 
       &.active {
+        color: ${primaryDark};
+        background-color: ${interactiveNormal};
         text-decoration: underline;
       }
     }
   }
 `;
 
-export const Logo = styled.h3`
-  height: 100%;
+export const Logo = styled.div`
+  height: 64px;
+  width: 64px;
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 2rem;
   margin-left: 20px;
-  padding: 0px;
 
   a {
-    color: black;
     text-decoration: none;
-    padding: 0px;
-    margin: 0;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `;
 
@@ -97,12 +124,39 @@ export const ConnectBtn = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: yellow;
+  background-color: ${interactiveNormal};
+  color: ${primaryDark};
   font-size: 1rem;
   padding: 10px;
   white-space: nowrap;
   margin-right: 20px;
   cursor: pointer;
+  border: none;
+  transition: background-color 0.3s, color 0.3s;
+
+  &:hover {
+    background-color: ${interactiveHover};
+    color: ${primaryDark};
+  }
+`;
+
+export const Btn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${interactiveNormal};
+  color: ${primaryDark};
+  font-size: 1rem;
+  padding: 10px;
+  white-space: nowrap;
+  cursor: pointer;
+  border: none;
+  transition: background-color 0.3s, color 0.3s;
+
+  &:hover {
+    background-color: ${interactiveHover};
+    color: ${primaryDark};
+  }
 `;
 
 export const Footer = styled.footer`
@@ -115,20 +169,29 @@ export const Footer = styled.footer`
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  background-color: grey;
+  color: black;
+  background-color: ${secondaryDark};
 `;
 
 export const Form = styled.form`
+  box-sizing: border-box;
   height: 600px;
   width: 400px;
   max-width: 600px;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: center;
-  background-color: blue;
-  margin-top: 20px;
+  background-color: ${secondaryLight};
+  margin-top: 84px;
   margin-bottom: 84px;
+  border-radius: 15px;
+  border: 5px solid ${interactiveNormal};
+
+  p {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
 
   @media (max-width: 992px) {
     margin-top: 0px;
@@ -137,25 +200,145 @@ export const Form = styled.form`
 `;
 
 export const Contract = styled.div`
-  height: 400px;
+  position: relative;
+  box-sizing: border-box;
+  height: 600px;
   width: 400px;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-around;
+  align-items: flex-start;
+  color: white;
+  background-image: url(/assets/contractBackground.png);
+  background-size: cover;
+  border-radius: 15px;
+  border: 5px solid white;
+  padding-left: 20px;
+  padding-right: 20px;
+
+  h2 {
+    margin-bottom: 5px;
+  }
+
+  h3 {
+    font-size: 1.75rem;
+    font-weight: 400;
+    margin-bottom: 5px;
+  }
+
+  p {
+    margin: 0;
+    color: ${interactiveNormal};
+  }
+`;
+
+export const FullAddress = styled.span`
+  display: none;
+  white-space: nowrap;
+  overflow: hidden;
+  position: absolute;
+  left: 100%;
+  transition: transform 0.3s ease-in-out;
+  align-self: flex-end;
+  cursor: pointer;
+  padding-top: 10px;
+  padding-bottom: 10px;
+`;
+
+export const ShortAddress = styled.span`
+  display: inline-block;
+  cursor: pointer;
+  align-self: flex-end;
+  padding-top: 10px;
+  padding-bottom: 10px;
+`;
+
+export const ContractHeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
   align-items: center;
-  background-color: red;
+  position: relative;
+  margin-top: 10px;
+
+  width: 100%;
+  font-size: 0.75rem;
+  font-weight: bold;
+  color: ${interactiveNormal};
+
+  &:hover ${FullAddress} {
+    display: inline-block;
+    transform: translateX(-100%);
+  }
+
+  &:hover ${ShortAddress} {
+    display: none;
+  }
+`;
+
+export const Amount = styled.span`
+  font-size: 1.125rem;
+  font-weight: bold;
+`;
+
+export const Creator = styled.span`
+  font-size: 1rem;
+  font-weight: bold;
+`;
+
+export const Adress = styled.span`
+  font-size: 1rem;
+  font-weight: 400;
 `;
 
 export const Text = styled.div`
+  box-sizing: border-box;
   height: 600px;
   width: 400px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: green;
+  background-color: ${secondaryLight};
   margin-top: 20px;
   margin-bottom: 84px;
+
+  padding-left: 20px;
+  padding-right: 20px;
+
+  border-radius: 15px;
+  border: 5px solid ${primaryLight};
+
+  @media (max-width: 992px) {
+    margin-top: 0px;
+    margin-bottom: 0px;
+  }
+`;
+
+export const DisclaimerText = styled.div`
+  box-sizing: border-box;
+  height: 600px;
+  width: 500px;
+  min-width: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  background-color: ${secondaryLight};
+  margin-top: 20px;
+  margin-bottom: 84px;
+
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-bottom: 20px;
+
+  border-radius: 15px;
+  border: 5px solid ${primaryLight};
+
+  h2,
+  h3 {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
 
   @media (max-width: 992px) {
     margin-top: 0px;
@@ -170,7 +353,13 @@ export const Image = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: purple;
+  background-color: ${primaryDark};
+  border-radius: 15px;
+  background-color: transparent;
+
+  img {
+    border-radius: 15px;
+  }
 `;
 
 export const Disclaimer = styled.div`
@@ -179,10 +368,79 @@ export const Disclaimer = styled.div`
   left: 0;
   width: 100%;
   min-height: calc(100vh - 128px);
-  background-color: green;
-  //background: rgba(0, 0, 0, 0.5);
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 64px;
+  z-index: 100;
+  background: repeating-linear-gradient(
+    135deg,
+    #e4d230,
+    #e4d230 40px,
+    ${primaryDark} 40px,
+    ${primaryDark} 80px
+  );
+`;
+
+export const Label = styled.label`
+  font-weight: bold;
+`;
+
+export const ClickableLabel = styled.label`
+  font-weight: bold;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 5px;
+  padding-right: 5px;
+`;
+
+export const CheckboxContainer = styled.div`
+  //margin-top: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: start;
+
+  input[type="checkbox"] {
+    appearance: none; // Removes default styling
+    -webkit-appearance: none; // Removes default styling for webkit browsers
+    background-color: #fff; // Default background
+    border: 1px solid #ddd; // Default border
+    padding: 9px; // Adjust size as needed
+    border-radius: 3px; // Optional: for rounded corners
+    display: inline-block;
+    position: relative;
+    margin-right: 10px;
+    cursor: pointer;
+
+    &:checked {
+      background-color: ${interactiveNormal};
+      border-color: ${interactiveNormal};
+    }
+
+    &:checked:after {
+      content: "✔";
+      position: absolute;
+      top: -2px;
+      left: 2px;
+      color: black;
+      font-size: 18px;
+    }
+  }
+
+  label {
+    cursor: pointer;
+    font-size: 1rem;
+  }
+`;
+
+export const CopiedNotification = styled.div`
+  position: absolute;
+  top: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: black;
+  color: white;
+  padding: 8px;
+  border-radius: 4px;
+  font-size: 0.8rem;
 `;
